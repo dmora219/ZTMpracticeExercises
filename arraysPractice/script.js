17293d7b7b56ea752f8b105d4ff7f84de9e444f6
dmora219 <dmora14633@gmail.com>
2023-06-01 15:16:18 -0500

    array practice

---
+++ arraysPractice/script.js
@@ -0,0 +1,16 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+array.shift();
+console.log(array);
+array.sort();
+console.log(array);
+array.push("Kiwi")
+console.log(array)
+array.shift();
+console.log(array)
+array.reverse();
+console.log(array);
+
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(array2[1][1])
