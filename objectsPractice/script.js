84bcaec6b281b1190426bd6d2d0473a0e9773684
dmora219 <dmora14633@gmail.com>
2023-06-02 23:49:18 -0500

    fb finished

--- objectsPractice/script.js
+++ objectsPractice/script.js
@@ -1,8 +1,52 @@
-let myObject = {
-   username: "dmora14633",
-   password: "dmora101",
-}
-let database = [myObject];
-let newsfeed = [myObject.username, myObject.password, database]
+let myObject = [
+   {
+      username: "dmora14633",
+      password: "dmora101",
+   },
+   {
+      username: "emo",
+      password: "ra101",
+   },
+   {
+      username: "joga",
+      password: "dmora1",
+   }
+]
+let newsFeed = [
+   {
+      username: "dmora14633",
+      newsfeed: "hello yall",
+   },
+   {
+      username: "emo",
+      newsfeed: "fuck you",
+   },
+   {
+      username: "joga",
+      newsfeed: "fuck emo",
+   }
+]
 
-console.log(newsfeed)
+let pass = prompt("enter password");
+let user = prompt("enter username");
+
+
+
+
+function validUser(username, password) {
+   for (let i = 0; i < myObject.length; i++){
+      if (myObject[i].username === username && myObject[i].password === password) {
+         return true
+      }
+   }
+   return false;
+}
+
+function signIn(username, password) {
+   if (validUser(username, password)) {
+      console.log(newsFeed)
+   } else {
+      alert('wrong password')
+   }
+}
+signIn(user, pass);
