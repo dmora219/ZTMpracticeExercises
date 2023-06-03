fec316b6800576fbfacc00b1ac93b3318162a171
dmora219 <dmora14633@gmail.com>
2023-06-02 20:47:10 -0500

    initial

---
+++ objectsPractice/script.js
@@ -0,0 +1,8 @@
+let myObject = {
+   username: "dmora14633",
+   password: "dmora101",
+}
+let database = [myObject];
+let newsfeed = [myObject.username, myObject.password, database]
+
+console.log(newsfeed)
